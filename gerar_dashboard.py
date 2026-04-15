242d262534052049d757776ac5798640ed011403
mggastal <matheusgastal@gmail.com>
2026-04-15 11:52:47 -0300

    Extend date range to last 330 days

--- gerar_dashboard.py
+++ gerar_dashboard.py
@@ -108,7 +108,7 @@ def build_daily(df):
         link_clicks=("link_clicks", "sum"),
     ).reset_index()
 
-    all_days = sorted(daily["date"].unique())[-90:]
+    all_days = sorted(daily["date"].unique())[-330:]
 
     out = {k: [] for k in [
         "days", "spend", "leads", "cpl", "ctr", "cpm",
